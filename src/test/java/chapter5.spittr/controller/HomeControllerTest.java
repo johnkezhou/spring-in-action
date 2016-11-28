1a3d7ce0b2f3731ede90e44143befcf40e3e6fbc
daoya <daoya@2dfire.com>
2016-11-28 11:04:10 +0800

    version

---
+++ src/test/java/chapter5.spittr/controller/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package chapter5.spittr.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+/**
+ * Created by daoya <daoya@2dfire.com> on 2016/11/28.
+ */
+public class HomeControllerTest {
+
+    @Test
+    public void testHomePage() throws Exception{
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
+
+}
